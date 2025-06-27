c4c801aaa344c239a9490265895d7343ba345618
SarahBoudarat <127493470+SarahBoudarat@users.noreply.github.com>
2025-06-27 11:29:09 +0200

    CoT-di-palma-test.py

--- CoT-di-palma.py
+++ CoT-di-palma.py
@@ -44,7 +44,7 @@ test_data_10 = pd.concat(test_rows).reset_index(drop=True)
 # -----------------------------
 # Chain-of-Thought Prompting
 # -----------------------------
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI()
 user_recommendations = defaultdict(dict)
 
 for user_id in sampled_users_10:
