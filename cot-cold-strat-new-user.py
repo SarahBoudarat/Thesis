3dedef022dafab9cb6148a1e6132b3c2ce867f20
SarahBoudarat <127493470+SarahBoudarat@users.noreply.github.com>
2025-06-27 11:46:59 +0200

    cot-cold-strat-new-user.py

--- cot-cold-strat-new-user.py
+++ cot-cold-strat-new-user.py
@@ -8,7 +8,7 @@ import openai
 # -----------------------------
 # SETUP
 # -----------------------------
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key="")
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # -----------------------------
@@ -54,7 +54,7 @@ with open("cold_start_train_data.pkl", "rb") as f:
 cold_start_train_data = cold_data["cold_start_train_data"]
 
 
-# Simulate cold-start: keep only top-3 rated movies per user in train set
+# Simulate cold-start: only top-3 rated movies per user in train set
 cold_start_train_rows = []
 
 for user_id in sampled_users_10:
