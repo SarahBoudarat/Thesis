635fe6ac0bae82d122cfbc1732c7589bfb79f7f4
SarahBoudarat <127493470+SarahBoudarat@users.noreply.github.com>
2025-06-27 12:15:42 +0200

    zero-shot-scale-up.py

--- zero-shot-scale-up.py
+++ zero-shot-scale-up.py
@@ -11,7 +11,7 @@ import csv
 # -----------------------------
 # SETUP
 # -----------------------------
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key="")
 model_name = "gpt-4-turbo"
 prompt_type = "zero-shot"
 
